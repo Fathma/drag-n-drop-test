cb0690f855de4965991c2cb5d641d0a39bc02a60
Fathma Siddique <fathmasiddique@Fathmas-MacBook-Air.local>
2021-03-20 20:41:25 +0600

    implemented react-beautiful-dnd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
+import './App.css';
+// import Draggable from 'react-draggable';
+const list = [
+  {
+    id: '1',
+    name: 'python',
+    
+  },
+  {
+    id: '2',
+    name: 'java',
+    
+  },
+  {
+    id: '3',
+    name: 'javascript',
+    
+  },
+  {
+    id: '4',
+    name: '.net',
+    
+  },
+  {
+    id: '5',
+    name: 'rubi',
+    
+  }
+]
 function App() {
+  const [characters, updateCharacters] = useState(list);
+
+  const handleOnDragEnd=(result)=> {
+    if (!result.destination) return;
+
+    const items = Array.from(characters);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+    console.log(items)
+    updateCharacters(items);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+    <DragDropContext onDragEnd={handleOnDragEnd}>
+      <Droppable droppableId="characters">
+      {(provided) => (
+      <ul className="characters" {...provided.droppableProps} ref={provided.innerRef}>
+        {characters.map((item, index)=>{
+          return (
+            <Draggable key={item.id} draggableId={item.id} index={index}>
+              {(provided) => (
+              <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                {item.name}
+              </li>
+              )}
+            </Draggable>
+          )
+        })}
+          {provided.placeholder}
+      </ul>
+      )}
+      </Droppable>
+    </DragDropContext>
+   </div>
   );
 }
 
